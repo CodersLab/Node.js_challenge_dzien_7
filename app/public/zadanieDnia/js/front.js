de47a664ab1d604d064fa52c0c9abdb31d642385
Ami777 <jakubkrol@o2.pl>
2018-02-02 11:01:36 +0100

    Typo fixed

--- app/public/zadanieDnia/js/front.js
+++ app/public/zadanieDnia/js/front.js
@@ -1,1 +1,89 @@
-// Twój kod
+$(function() {
+  const input = $('input.new-todo');
+  const list = $('ul.todo-list');
+
+  const update = (ans) => {
+    console.log(ans.todos);
+    list.empty();
+    ans.todos.forEach(item => {
+      const completed = item.complete === true && 'class="completed"';
+      list.append(`<li ${completed}>
+            <div class="view">
+                <input class="toggle" type="checkbox">
+                <label>${item.todo}</label>
+                <button class="destroy"></button>
+            </div>
+            <input class="edit" value="Create a TodoMVC template">
+        </li>`);      
+    });
+    
+    $('input.toggle').on('change', function() {
+      const i = $(this).index('input.toggle');
+      console.log('toggle id', i);
+      $.ajax({
+          url: '/toggle',
+          data: JSON.stringify({
+            i,
+          }),
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          type: 'POST',
+          dataType: 'json',
+        })
+        .then(ans => update(ans));
+    });
+    
+    $('button.destroy').click(function() {
+      const i = $(this).index('button.destroy');
+      console.log('destroy id', i);
+      $.ajax({
+          url: '/destroy',
+          data: JSON.stringify({
+            i,
+          }),
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          type: 'POST',
+          dataType: 'json',
+        })
+        .then(ans => update(ans));
+    });
+    
+  }
+
+  $.ajax({
+      url: '/show',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      type: 'GET',
+      dataType: 'json',
+    })
+    .then(ans => update(ans));
+
+  input.on('keypress', (e) => {
+    if (e.target.value && e.keyCode == 13) {
+      const todo = input.val();
+      input.val("");
+
+      $.ajax({
+          url: '/add',
+          data: JSON.stringify({
+            todo,
+            complete: false
+          }),
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          type: 'POST',
+          dataType: 'json',
+        })
+        .then(ans => update(ans));
+    }
+  });
+
+
+
+});
--- app/zadanieDnia.js
+++ app/zadanieDnia.js
@@ -1,1 +1,98 @@
-//Twój kod
+const fs = require('fs');
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(bodyParser.json());
+app.use(express.static('./public/zadanieDnia/'));
+
+const DB_FILE = './data/zadanieDnia/db.json';
+
+app.get('/show', (req, res) => {
+  fs.readFile(DB_FILE, (err, data) => {
+    if (!err) {
+      const todos = JSON.parse(data);
+      res.json({
+        todos
+      });
+    } else {
+      console.log('Błąd odczytu pliku', err);
+    }
+  });
+});
+
+app.post('/add', (req, res) => {
+  fs.readFile(DB_FILE, (err, data) => {
+    if (!err) {
+      const todos = JSON.parse(data);
+      todos.push({
+        todo: req.body.todo,
+        complete: req.body.complete
+      });
+      const updateTodos = JSON.stringify(todos);
+
+      fs.writeFile(DB_FILE, updateTodos, (err, data) => {
+        if (!err) {
+          res.json({
+            todos
+          });
+          console.log('Added Todo:', req.body.todo);
+        } else {
+          console.log('Błąd zapisu pliku', err);
+        }
+      });
+    } else {
+      console.log('Błąd odczytu pliku', err);
+    }
+  });
+});
+
+app.post('/toggle', (req, res) => {
+  fs.readFile(DB_FILE, (err, data) => {
+    if (!err) {
+      const todos = JSON.parse(data);
+      todos[req.body.i].complete = !todos[req.body.i].complete;
+      const updateTodos = JSON.stringify(todos);
+
+      fs.writeFile(DB_FILE, updateTodos, (err, data) => {
+        if (!err) {
+          res.json({
+            todos
+          });
+          console.log('Toggle Todo nr :', req.body.i);
+        } else {
+          console.log('Błąd zapisu pliku', err);
+        }
+      });
+    } else {
+      console.log('Błąd odczytu pliku', err);
+    }
+  });
+});
+
+app.post('/destroy', (req, res) => {
+  fs.readFile(DB_FILE, (err, data) => {
+    if (!err) {
+      const todos = JSON.parse(data);
+      todos.splice(req.body.i, 1);
+      const updateTodos = JSON.stringify(todos);
+
+      fs.writeFile(DB_FILE, updateTodos, (err, data) => {
+        if (!err) {
+          res.json({
+            todos
+          });
+          console.log('Removed Todo nr :', req.body.i);
+        } else {
+          console.log('Błąd zapisu pliku', err);
+        }
+      });
+    } else {
+      console.log('Błąd odczytu pliku', err);
+    }
+  });
+});
+
+app.listen(3000, () => {
+  console.log('Serwer uruchomiony na porcie 3000');
+});
