f8dc4d4d04a74ef97b886827c8ca7adac8c723c5
Łukasz Dadlez <lukaszdadlez@gmail.com>
2018-02-05 23:10:21 +0100

    first draft list updates from input

--- app/public/zadanieDnia/js/front.js
+++ app/public/zadanieDnia/js/front.js
@@ -1,1 +1,41 @@
 // Twój kod
+$(() => {
+  console.log('skrypt załadowany');
+
+  const newTask = $('.new-todo');
+  const list = $('.todo-list');
+  const firstItem = list.firstItem
+
+  const clearValue = (element) => element.val('');
+  const addItem = (value) => {
+    list.children().first().prepend($(`
+    <li>
+      <div class="view">
+        <input class="toggle" type="checkbox">
+        <label>${value}</label>
+        <button class="destroy"></button>
+      </div>
+      <input class="edit" value="Rule the web">
+    </li>
+    `))
+  };
+
+  $(document).keypress(function(e) {
+    if(e.which == 13) {
+        $.ajax({
+          data: JSON.stringify({
+            id: 1,
+            text: newTask.val(),
+          }),
+          headers: {
+              'Content-Type': 'application/json',
+          },
+          type : 'POST',
+          dataType : 'json'
+        }).then(ans => {
+          console.log(ans);
+          addItem(newTask.val());
+        });
+    }
+  });
+});
--- app/zadanieDnia.js
+++ app/zadanieDnia.js
@@ -1,1 +1,15 @@
 //Twój kod
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(express.static('./app/public/zadanieDnia/')); //czemu w przykładzie wystarczy ścieżka względna do przykladAjax.js, a tutaj potrzebuję ścieżki względem node-modules?
+
+app.post('/', (req, res) => {
+  console.log('req.body', req.body);
+  res.json('ok');
+});
+
+app.listen(3000, () => console.log('serwer stoi na porcie 3000'));
