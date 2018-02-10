6350f3ff602ec436c21fbc66f81b94fd102b296d
Gladkowski <marcingladkowski@gmail.com>
2018-02-10 11:20:46 +0100

    display list in main view from backend

--- app/public/zadanieDnia/js/front.js
+++ app/public/zadanieDnia/js/front.js
@@ -1,1 +1,36 @@
-// Twój kod
+$(document).ready(function() {
+
+    var taskListUrl = '/task/list';
+    var listUl  = $('.todo-list');
+
+    function readData(url) {
+        $.ajax({
+            url : url,
+            dataType : 'json'
+        })
+        .done(ret => {
+            console.log(ret);
+
+            ret.tasks.forEach(element => {
+               createTaskLi(element); 
+            });
+        });
+    }
+
+    readData(taskListUrl);
+
+    function createTaskLi(task){
+        let li = $(`
+                <li>
+                    <div class="view">
+                        <input class="toggle" type="checkbox">
+                        <label>${task.title}</label>
+                        <button class="destroy"></button>
+                    </div>
+                    <input class="edit" value="Rule the web">
+                </li>
+            `);
+        listUl.append(li);
+    }
+
+});
